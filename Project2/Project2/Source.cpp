4eacf6299f35c05517d16b596d0548d3842bc140
user <user@A34COMP6>
2018-01-27 16:23:37 +0200

    2

--- Project2/Project2/Source.cpp
+++ Project2/Project2/Source.cpp
@@ -4,6 +4,21 @@ using namespace std;
 
 int main()
 {
+	string text;
+	getline(cin, text);
+	for (int i = 0; i < text.length(); ++i) {
+		if (isupper(text[i])) {
+			text[i]=(char)(tolower(text[i]));
+		}
+		else if (islower(text[i])) {
+			text[i] = (char)toupper(text[i]);
+		}
 
+	}
+	cout << text << endl;
+	for (int i = 0; i < text.size(); ++i) {
+		cout << text[i] << " ";
+	}
+	cout << endl;
 	return 0;
 }
