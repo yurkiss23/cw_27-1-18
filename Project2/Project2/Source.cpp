5e5ff062ad49771607f76c9bc257da914ff997c0
user <user@A34COMP6>
2018-01-27 17:00:07 +0200

    end

--- Project2/Project2/Source.cpp
+++ Project2/Project2/Source.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
-#include<string>
+//#include<string>
 using namespace std;
 
 int main()
 {
-	string text;
+	/*string text;
 	getline(cin, text);
 	for (int i = 0; i < text.length(); ++i) {
 		if (isupper(text[i])) {
@@ -19,6 +19,20 @@ int main()
 	for (int i = 0; i < text.size(); ++i) {
 		cout << text[i] << " ";
 	}
-	cout << endl;
+	cout << endl;*/
+
+	int sum = 0, mult = 1;
+	const int ROWS = 3, COLS = 4;
+	int m[ROWS][COLS] = { {1,2},{3,4},5,6,7 };
+	for (int r = 0; r < ROWS; ++r) {
+		for (int i = 0; i < COLS; ++i) {
+			sum += m[r][i];
+			mult *= m[r][i];
+			cout << m[r][i] << "\t";
+		}
+		cout << endl;
+	}
+	
+	//cout <<"\nsum: "<< sum << "\tmult: " << mult << endl;
 	return 0;
 }
